4ac6ce56eec08233c4ac7bb0f8bc414ae38c765d
Truls Karlsen <126862322+trulsk@users.noreply.github.com>
2026-04-23 10:47:34 +0200

    Update sftp_download.sh

--- Downloading/sftp_download.sh
+++ Downloading/sftp_download.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+
+# USERNAME, PASSWORD, PATH/TO/IDs.txt and PATH/TO/DOWNLOADS/ must be set manually
+
 while read p; do
         echo  "Downloading $p"
-        curl -k -l -s "sftp://ftp.gportal.jaxa.jp:2051/$p/" --user "USERNAME:PASSWORD" >> PATH/TO/IDS.TXT
+        curl -k -l -s "sftp://ftp.gportal.jaxa.jp:2051/$p/" --user "USERNAME:PASSWORD" >> PATH/TO/IDs.txt
         while read q; do
 
                 if [ "$q" != "." ] && [ "$q" != ".." ];
